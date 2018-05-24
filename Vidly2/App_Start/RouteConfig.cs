bf4c82fcac7413fa7b620551d5f695292d417a7c
RaoufEd <raouf.edabbar@gmail.com>
2018-05-24 05:12:12 +0100

    Add project files.

---
+++ Vidly2/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Define custom route
+            //but its an old methode and messy check below to find a better way !!!!
+            //which is attribute routing
+
+            //new way
+            routes.MapMvcAttributeRoutes();
+
+
+            /*
+            //**********************old way********************
+             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "Movie/Released/{year}/{month}",
+                new{controller = "Movie", action ="ByReleaseDate"},
+                //adding special parameter d means digits !
+
+                new{year=@"\d{4}" , month =@"\d{2}"}
+            );
+
+           */
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
